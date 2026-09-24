9ab7089780b4ad1de8a63f2c8bb69bc5392030f2
dicleinceleruysal <dicleinceleruysal@gmail.com>
2026-09-24 15:34:25 +0300

    fix(vercel): Rename client script to client.js to prevent Vercel from treating browser code as Node root entrypoint, set node engine to 20.x

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const STATIC_ASSETS = [
   './',
   './index.html',
   './style.css',
-  './app.js',
+  './client.js',
   './manifest.json',
   './icons/icon.svg',
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap',
